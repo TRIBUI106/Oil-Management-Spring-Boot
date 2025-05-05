849701f26ae2f4c2dfa3c69c3716907b2219c259
chez1s <toptrainghieml@gmail.com>
2025-05-05 23:10:35 +0700

    Update Product.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/entity/Product.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/entity/Product.java
@@ -1,4 +1,16 @@
 package com.chez1s.QuanLyKhoNhot.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+//@Entity //Có thể bật lên nếu muốn tự động tạo bảng theo entity có 
 public class Product {
 }
