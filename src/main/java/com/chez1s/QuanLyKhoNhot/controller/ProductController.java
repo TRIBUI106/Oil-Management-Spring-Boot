114845efcde28cf39027112a9aadd8ac21a04636
chez1s <toptrainghieml@gmail.com>
2025-05-05 23:54:40 +0700

    Update ProductController.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/controller/ProductController.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.chez1s.QuanLyKhoNhot.controller;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// Báo hiệu thằng này là 1 cái Controller, tức là RestAPI á
+// Báo hiệu thằng này là 1 cái Controller, tức là RestAPI
+@RequestMapping("oil")
+// Cái này là cái gì thì tôi không biết
 public class ProductController {
 
+        private final Product
 
 
 }
