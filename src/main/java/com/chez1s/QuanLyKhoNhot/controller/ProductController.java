231c60d26cfe6575715f8abe467427ede72cc8d4
chez1s <toptrainghieml@gmail.com>
2025-05-05 23:51:28 +0700

    Create ProductController.java

---
+++ src/main/java/com/chez1s/QuanLyKhoNhot/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.chez1s.QuanLyKhoNhot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// Báo hiệu thằng này là 1 cái Controller, tức là RestAPI á
+public class ProductController {
+
+
+
+}
