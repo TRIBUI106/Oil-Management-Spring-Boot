55f5949ad11107f255d9e513527d4d081e85f4da
chez1s <toptrainghieml@gmail.com>
2025-05-08 23:44:24 +0700

    Update ProductResponseDto.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/dto/response/ProductResponseDto.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/dto/response/ProductResponseDto.java
@@ -1,8 +1,12 @@
 package com.chez1s.QuanLyKhoNhot.dto.response;
 
 import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductResponseDto {
 
