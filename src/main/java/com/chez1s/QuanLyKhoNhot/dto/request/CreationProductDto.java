6b39f73178fb8a5633b456c34aa2c1de96aa48a4
chez1s <toptrainghieml@gmail.com>
2025-05-08 23:44:36 +0700

    Update CreationProductDto.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/dto/request/CreationProductDto.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/dto/request/CreationProductDto.java
@@ -1,4 +1,13 @@
 package com.chez1s.QuanLyKhoNhot.dto.request;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreationProductDto {
+
 }
