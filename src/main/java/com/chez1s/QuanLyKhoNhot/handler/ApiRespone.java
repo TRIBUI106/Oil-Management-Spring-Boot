47eeff16c4a38f7ad331784c847bddf2c1be8f36
chez1s <toptrainghieml@gmail.com>
2025-05-08 22:29:56 +0700

    Thêm package handler và api respone

---
+++ src/main/java/com/chez1s/QuanLyKhoNhot/handler/ApiRespone.java
@@ -0,0 +1,15 @@
+package com.chez1s.QuanLyKhoNhot.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+// @Data nó là cả @Getter @Setter nha
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiRespone<T> {
+
+}
