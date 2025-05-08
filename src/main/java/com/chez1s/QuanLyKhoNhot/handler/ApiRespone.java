fc709de95c92f75f176c8405a70b0dd596fe4a13
chez1s <toptrainghieml@gmail.com>
2025-05-08 22:31:34 +0700

    Update ApiRespone.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/handler/ApiRespone.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/handler/ApiRespone.java
@@ -1,16 +1,15 @@
 package com.chez1s.QuanLyKhoNhot.handler;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 //@Data
-//// @Data nó là cả @Getter @Setter @toString @Equals g đó
-
+//// @Data nó là cả @Getter @Setter @toString @EqualsAndHashCode, @RequiredArgsConstructor
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class ApiRespone<T> {
 
 }
