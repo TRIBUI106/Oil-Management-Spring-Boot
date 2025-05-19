af47b403f7faa140585191024c7fb497cd6877e3
chez1s <toptrainghieml@gmail.com>
2025-05-19 22:03:55 +0700

    Update Brand.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/entity/Brand.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/entity/Brand.java
@@ -1,11 +1,16 @@
 package com.chez1s.QuanLyKhoNhot.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
-@no
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults()
 public class Brand {
 
     private int id;
