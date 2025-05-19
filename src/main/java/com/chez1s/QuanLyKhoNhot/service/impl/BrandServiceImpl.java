0ba30f04023c5fef3c7077c475ded4024cc48481
chez1s <toptrainghieml@gmail.com>
2025-05-19 22:34:26 +0700

    Connect <3

--- src/main/java/com/chez1s/QuanLyKhoNhot/service/impl/BrandServiceImpl.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/service/impl/BrandServiceImpl.java
@@ -1,11 +1,14 @@
 package com.chez1s.QuanLyKhoNhot.service.impl;
 
+import com.chez1s.QuanLyKhoNhot.entity.Brand;
 import com.chez1s.QuanLyKhoNhot.mapper.BrandMapper;
 import com.chez1s.QuanLyKhoNhot.service.BrandService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BrandServiceImpl implements BrandService {
@@ -14,5 +17,10 @@ public class BrandServiceImpl implements BrandService {
 
     public BrandServiceImpl(BrandMapper brandMapper) {
         this.brandMapper = brandMapper;
+    }
+
+    @Override
+    public List<Brand> getAll() {
+        return brandMapper.getAll();
     }
 }
