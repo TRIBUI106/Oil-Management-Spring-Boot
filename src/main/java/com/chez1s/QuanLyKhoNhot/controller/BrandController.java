800e30200b459ae062a705ba1d124fab8fd9bb56
chez1s <toptrainghieml@gmail.com>
2025-05-19 22:39:43 +0700

    Update BrandController.java

--- src/main/java/com/chez1s/QuanLyKhoNhot/controller/BrandController.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/controller/BrandController.java
@@ -1,5 +1,9 @@
 package com.chez1s.QuanLyKhoNhot.controller;
 
+import com.chez1s.QuanLyKhoNhot.handler.ApiRespone;
+import com.chez1s.QuanLyKhoNhot.service.BrandService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/brands")
 public class BrandController {
 
-    
+    private final BrandService brandService;
+
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping
+    public ResponseEntity responseEntity () {
+        
+    }
 
 }
