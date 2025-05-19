0ab5ecb48612ff8fd648eb4733c99a7b152edfd8
chez1s <toptrainghieml@gmail.com>
2025-05-19 22:01:37 +0700

    Create Brand.java

---
+++ src/main/java/com/chez1s/QuanLyKhoNhot/entity/Brand.java
@@ -0,0 +1,14 @@
+package com.chez1s.QuanLyKhoNhot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@no
+public class Brand {
+
+    private int id;
+    private String name;
+
+}
