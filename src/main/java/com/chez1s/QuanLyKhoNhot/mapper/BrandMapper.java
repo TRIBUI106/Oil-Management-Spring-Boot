6a19d3779682cf9b159ce06606fd542403d0a067
chez1s <toptrainghieml@gmail.com>
2025-05-19 22:19:27 +0700

    Fixing stuffs in mapper mdfk

--- src/main/java/com/chez1s/QuanLyKhoNhot/mapper/BrandMapper.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/mapper/BrandMapper.java
@@ -1,14 +1,15 @@
 package com.chez1s.QuanLyKhoNhot.mapper;
 
+import com.chez1s.QuanLyKhoNhot.entity.Brand;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
-public class BrandMapper {
+public interface BrandMapper {
 
-    @Select(SELECT id, brand from brands)
-    List getAll
+    @Select("SELECT id, brand from brands")
+    List<Brand> findAll();
 
 }
