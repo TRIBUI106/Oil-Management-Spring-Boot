63f992dc4a1480fb205d427814e301d7a4584f68
chez1s <toptrainghieml@gmail.com>
2025-05-26 22:05:16 +0700

    Update ProductMapper.java
    Co-Authored-By: Nguyen Quang Minh <137589157+NguyenMinh1301@users.noreply.github.com>

--- src/main/java/com/chez1s/QuanLyKhoNhot/mapper/ProductMapper.java
+++ src/main/java/com/chez1s/QuanLyKhoNhot/mapper/ProductMapper.java
@@ -2,12 +2,16 @@ package com.chez1s.QuanLyKhoNhot.mapper;
 
 import com.chez1s.QuanLyKhoNhot.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
     List<Product> getAllProduct();
+
     void insertProduct(Product product);
+
     void deleteProductById(Integer id);
+
     void updateProductById(Product product);
 }
