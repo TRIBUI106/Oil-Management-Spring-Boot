cfb074a4e1ab8393b28bb6de96e2d4c5fb088d72
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-07-05 12:00:44 +0700

    feat/inital-pageableResponse

---
+++ src/main/java/com/chez1s/QuanLyKhoNhot/handler/PageableResponse.java
@@ -0,0 +1,20 @@
+package com.chez1s.QuanLyKhoNhot.handler;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PageableResponse<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+
+}
